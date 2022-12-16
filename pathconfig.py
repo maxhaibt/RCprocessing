b34210864207d2e05d74817884dc282fc6bcc251
mhaibt <max.haibt@dainst.de>
2022-12-16 11:45:29 +0100

    config setup

--- pathconfig.py
+++ pathconfig.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 RTpath= Path('C:/Program Files/RawTherapee/5.8/rawtherapee-cli.exe')
 RCpath= Path('C:/Program Files/Capturing Reality/RealityCapture/RealityCapture.exe')
 messagepath= Path('D:/3DPhotogrammetry/WES_L18_Boat1/RCmessages')
