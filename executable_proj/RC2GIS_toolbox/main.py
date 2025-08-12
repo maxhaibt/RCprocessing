78cca7be0b7b203d94749c763f0fc9c54b04156f
maxhaibt <max.haibt@dainst.de>
2025-08-12 08:13:48 +0200

    KI fix for image dem confusion

--- executable_proj/RC2GIS_toolbox/main.py
+++ executable_proj/RC2GIS_toolbox/main.py
@@ -14,11 +14,18 @@ def main():
     btn_Orthobox2GIS = tk.Button(root, text="Orthobox2GIS", command=lambda: toolbox.open_file_browser_Orthobox2GIS(root))
     btn_Orthobox2GIS.pack(pady=20)  # Add some padding around the button
 
-    btn_hugeTiffs2GIS = tk.Button(root, text="hugeTiffs2GIS", command=lambda: toolbox.open_file_browser_hugeTiffs2GIS( root))
-    btn_hugeTiffs2GIS.pack(pady=20)  # Add some padding around the button#
+
 
     btn_hugeTiffs2GIS = tk.Button(root, text="sideviewTiffs2GIS", command=lambda: toolbox.open_file_browser_sideviewTiffs2GIS(root))
     btn_hugeTiffs2GIS.pack(pady=20)  # Add some padding around the button
+
+    btn_hugeTiffs2GIS_dem = tk.Button(root, text="hugeTiffs2GIS - DEM",
+                                  command=lambda: toolbox.open_file_browser_hugeTiffs2GIS_dem(root))
+    btn_hugeTiffs2GIS_dem.pack(pady=10)
+
+    btn_hugeTiffs2GIS_true = tk.Button(root, text="hugeTiffs2GIS - Truecolor",
+                                    command=lambda: toolbox.open_file_browser_hugeTiffs2GIS_truecolor(root))
+    btn_hugeTiffs2GIS_true.pack(pady=10)
 
     root.mainloop()
 print("Running main.py")
